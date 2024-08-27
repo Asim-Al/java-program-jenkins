4f299b171f401cd32478f2fa047adf7abeeb580f
Asim-Al <saifi.asim55@gmail.com>
2024-08-27 16:40:33 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public static void main(String[]args){
     System.out.println("This is java program");
     System.out.println("Output is from java program");
     System.out.println(new Date());
+    System.out.println("hello");
 }
 }
